fc7bbbc89eb55aea184359cf2a499f2dd88cfa69
KiltechOB <olegburda2@gmail.com>
2023-10-25 13:12:30 +0300

    Added Feature1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             // Code was edidet in GitHub
             // Code was added in VS
+            // Code to call Feature 1
         }
     }
 }
