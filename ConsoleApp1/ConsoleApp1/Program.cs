7e92b08c4f2f2dedf0588ce175ea9afef4c3d54a
Oleh <57638535+KiltechOB@users.noreply.github.com>
2023-10-25 14:09:01 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,10 @@
             Console.WriteLine("Hello, World!");
             // Code was edidet in GitHub
             // Code was added in VS
+            
             // Code to call Feature 1
+
+            // Code to call Feature 3
         }
     }
 }
