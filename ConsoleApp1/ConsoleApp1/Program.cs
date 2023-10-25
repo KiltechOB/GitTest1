ff30c5da73e2baec231bf26b6e16d43f459d1799
KiltechOB <olegburda2@gmail.com>
2023-10-25 12:29:41 +0300

    1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
             Console.WriteLine("Hello, World!");
             // Code was added in GitHub
